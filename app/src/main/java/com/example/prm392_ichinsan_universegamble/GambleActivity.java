baa1a706f3e68de298463de6954bc5123559b9c4
phuongnguyen521 <ngcphungnguyn@gmail.com>
2022-05-25 10:58:29 +0700

    Initial commit

---
+++ app/src/main/java/com/example/prm392_ichinsan_universegamble/GambleActivity.java
@@ -0,0 +1,14 @@
+package com.example.prm392_ichinsan_universegamble;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GambleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gamble);
+    }
+}
